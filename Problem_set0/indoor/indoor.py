380cd3cb4f7cebd845f7af4ab244d679f07c9b47
joshuakalungi <joshuakalungi101@gmail.com>
2023-10-18 00:35:47 +0300

    indoor

---
+++ Problem_set0/indoor/indoor.py
@@ -0,0 +1,10 @@
+# indoor.py
+
+# Prompt the user for input
+user_input = input("Enter a string: ")
+
+# Convert the input to lowercase
+lowercase_input = user_input.lower()
+
+# Output the lowercase input
+print("Lowercase version: " + lowercase_input)
